9146f7d46629881b3ee9480a17c23a91da700fc4
a2k-Oleksii <a2k-oleksii@ukr.net>
2023-01-19 21:57:25 +0200

    Add view home (request cities.home.html)

--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from cities.models import City
 
-# Create your views here.
+
+def home(request):
+    cities = City.objects.all()
+    context = {"objects_list": cities, }
+    return render(request, 'cities/home.html', context)
