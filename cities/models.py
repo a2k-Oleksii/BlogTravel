e3d75807506927ca1ed8f996059833a401b3d439
a2k-Oleksii <a2k-oleksii@ukr.net>
2023-01-19 21:21:58 +0200

    Add in model City verbose_name, class Meta and __str__ method.

--- cities/models.py
+++ cities/models.py
@@ -2,4 +2,12 @@ from django.db import models
 
 
 class City(models.Model):
-    name = models.CharField(max_length=150, unique=True)
+    name = models.CharField(max_length=150, unique=True, verbose_name="Місто")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Місто"
+        verbose_name_plural = "Містa"
+        ordering = ["name"]
