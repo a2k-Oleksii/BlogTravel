b7765c11bf7ed3ede582bdf2fef46f3fc5ac422f
a2k-Oleksii <a2k-oleksii@ukr.net>
2023-02-04 15:30:15 +0200

    Add functions create, delete, and update.

--- cities/views.py
+++ cities/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 from .forms import CityForm
 from cities.models import City
@@ -19,3 +21,26 @@ class CityDetailView(DetailView):
     queryset = City.objects.all()
     context_object_name = "city"
     template_name = "cities/detail.html"
+
+
+class CityCreateView(CreateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/create.html'
+    success_url = reverse_lazy('cities:home')
+
+
+class CityUpdateView(UpdateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/update.html'
+    success_url = reverse_lazy('cities:home')
+
+
+class CityDeleteView(DeleteView):
+    model = City
+    template_name = "cities/delete.html"
+    success_url = reverse_lazy('cities:home')
+
+    # def get(self, request, *args, **kwargs):
+    #     return self.post(request, *args, **kwargs)
