f82790e9cd9f2550ba8a31f3e2e78ca5b810e154
a2k-Oleksii <a2k-oleksii@ukr.net>
2023-02-07 21:16:21 +0200

    Create model Train, create migrations and add in admin

--- trains/models.py
+++ trains/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Train(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name="Номер потяга")
+    from_city = models.ForeignKey("cities.City", on_delete=models.CASCADE, verbose_name="Звідки", related_name="from_city")
+    to_city = models.ForeignKey("cities.City", on_delete=models.CASCADE, verbose_name="Куди", related_name="to_city")
+    travel_time = models.IntegerField(verbose_name="Час в дорозі")
+
+    class Meta:
+        verbose_name = "Потяг"
+        verbose_name_plural = "Потяги"
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name} з м.{self.from_city} в м.{self.to_city}"
